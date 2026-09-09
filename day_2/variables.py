3fdd77638db37b1f47e96fcc562858f64de9ed9d
Ho Dinh Anh <hodinhanh1234@gmail.com>
2026-09-09 16:33:02 +0700

    Add day2

---
+++ day_2/variables.py
@@ -0,0 +1,43 @@
+# Ngày 2: 30 ngày lập trình Python
+#Level 1
+first_name ='Dinh Anh'
+last_name = 'Ho'
+name = 'Ho Dinh Anh'
+country ='Viet Nam'
+city ='Ha Noi'
+age =20
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+#Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(is_married))
+print(len(name))
+print( len(name),'||',len(first_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+area_of_circle = 3.14 * 30 ** 2
+circum_of_circle = 2 * 3.14 * 30
+r = input('Enter radius: ')
+name = input('Enter your name: ')
+print(name)
+last_name = input('Enter your last name: ')
+country = input('Enter your country: ')
+age = input('Enter your age: ')
+
+help('keywords')
+
